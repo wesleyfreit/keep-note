415fafa01e65659c535cc51bb01dfd2c84443aa5
Richard Freitas <wesleyrichard30@gmail.com>
2024-02-09 06:17:09 -0300

    feat: add styles in new note card and note card components

--- apps/web/src/components/new-note-card.tsx
+++ apps/web/src/components/new-note-card.tsx
@@ -1,11 +1,49 @@
+'use client';
+import * as Dialog from '@radix-ui/react-dialog';
+import { X } from 'lucide-react';
+
 export const NewNoteCard = () => {
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-4">
-      <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md bg-slate-700 text-left p-5 space-y-4 flex flex-col hover:ring-2 outline-none hover:ring-slate-500 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
 
-      <p className="text-sm leading-6 text-slate-400">
-        Escreva algo ou grave um áudio para ser convertido em texto
-      </p>
-    </div>
+        <p className="text-sm leading-6 text-slate-400">
+          Escreva algo ou grave um áudio para ser convertido em texto
+        </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/50 flex items-center justify-center">
+          <Dialog.Content className="bg-slate-700 relative overflow-hidden rounded-md flex flex-col w-full h-full max-w-[640px] max-h-[60vh] outline-none overflow-y-auto">
+            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100  rounded-bl-md">
+              <X className="size-5" />
+            </Dialog.Close>
+
+            <div className="flex flex-1 flex-col gap-5 p-5">
+              <span className="text-sm font-medium text-slate-300">Adicionar nota</span>
+
+              <div className="overflow-y-auto max-h-[45vh]">
+                <p className="text-sm leading-6 text-slate-400">
+                  Comece{' '}
+                  <button className="font-medium text-lime-400 hover:underline">
+                    escrevendo um texto
+                  </button>{' '}
+                  ou{' '}
+                  <button className="font-medium text-lime-400 hover:underline">
+                    gravando uma nota
+                  </button>{' '}
+                  em áudio.
+                </p>
+              </div>
+            </div>
+
+            <button className="text-center outline-none text-sm font-medium bg-lime-400 text-lime-950 py-4 transition-colors hover:bg-lime-500">
+              Salvar nota
+            </button>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
--- apps/web/src/components/note-card.tsx
+++ apps/web/src/components/note-card.tsx
@@ -1,18 +1,51 @@
-export const NoteCard = () => {
+'use client';
+import * as Dialog from '@radix-ui/react-dialog';
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { X } from 'lucide-react';
+
+interface NoteCardProps {
+  note: { date: Date; content: string };
+}
+
+export const NoteCard = ({ note }: NoteCardProps) => {
   return (
-    <button className="rounded-md bg-slate-800 text-left p-5 space-y-4 overflow-hidden relative hover:ring-2 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md bg-slate-800 text-left p-5 space-y-4 overflow-hidden relative hover:ring-2 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+        </span>
 
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ad possimus
-        voluptatem voluptate ipsam cum voluptas repudiandae quia optio error quas sed
-        natus, velit, eveniet enim explicabo? Id, rem officia. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Aperiam, ad possimus voluptatem voluptate ipsam cum
-        voluptas repudiandae quia optio error quas sed natus, velit, eveniet enim
-        explicabo? Id, rem officia.
-      </p>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/50 flex items-center justify-center">
+          <Dialog.Content className="bg-slate-700 relative overflow-hidden rounded-md flex flex-col w-full h-full max-w-[640px] max-h-[60vh] outline-none overflow-y-auto">
+            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100  rounded-bl-md">
+              <X className="size-5" />
+            </Dialog.Close>
+
+            <div className="flex flex-1 flex-col gap-5 p-5">
+              <span className="text-sm font-medium text-slate-300">
+                {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+              </span>
+
+              <div className="overflow-y-auto max-h-[45vh]">
+                <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+              </div>
+            </div>
+
+            <button className="text-center outline-none text-sm font-medium bg-slate-800 text-slate-300 py-4 transition-colors group">
+              Deseja{' '}
+              <span className="text-red-400 group-hover:underline">apagar esta nota</span>
+              ?
+            </button>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
