1486905a06eb2a2c46aacc587b0ebee714b34b4c
Richard Freitas <wesleyrichard30@gmail.com>
2024-02-12 13:49:35 -0300

    feat: add empty notes/search component

--- apps/web/src/components/empty-notes.tsx
+++ apps/web/src/components/empty-notes.tsx
@@ -1,11 +1,17 @@
 import { StickyNote } from 'lucide-react';
 
-export const EmptyNotes = () => {
+interface EmptyNotesProps {
+  searchResult: boolean;
+}
+
+export const EmptyNotes = ({ searchResult }: EmptyNotesProps) => {
   return (
     <div className="flex h-96 flex-grow items-center justify-center flex-col ">
       <StickyNote className="h-24 w-24 text-slate-700" />
       <span className="text-slate-500 font-medium text-xl">
-        As notas adicionadas serão exibidas aqui
+        {searchResult
+          ? 'Nenhum resultado encontrado'
+          : 'As notas adicionadas serão exibidas aqui'}
       </span>
     </div>
   );
