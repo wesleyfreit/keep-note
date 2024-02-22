3349d36eae09de738c76827b179adc1f32cae191
Richard Freitas <wesleyrichard30@gmail.com>
2024-02-22 20:55:05 -0300

    refactor: remove smtp validation because it works wrong

--- apps/api/src/lib/email-validator.ts
+++ apps/api/src/lib/email-validator.ts
@@ -1,17 +1,5 @@
 import validate from 'deep-email-validator';
 
 export const emailValidator = async (email: string) => {
-  let validateSMTP = true;
-
-  const domain = email.split('@')[1];
-
-  const isMicrosoftDomain = domain
-    ? /(hotmail|outlook)\.com(\.br)?$/.test(domain)
-    : false;
-
-  if (isMicrosoftDomain) {
-    validateSMTP = false;
-  }
-
-  return await validate({ email, validateSMTP });
+  return await validate({ email, validateSMTP: false });
 };
