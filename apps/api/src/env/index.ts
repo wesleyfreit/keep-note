db2f3e8dedb2b754d972c2488ec2220dccf51d5a
Richard Freitas <wesleyrichard30@gmail.com>
2024-02-22 22:13:45 -0300

    refactor: move env validation schema for env index directory

--- apps/api/src/env/index.ts
+++ apps/api/src/env/index.ts
@@ -1,5 +1,17 @@
-import { envSchema } from '@/validation/env-schema';
 import 'dotenv/config';
+import z from 'zod';
+
+export const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+  ORIGIN_URL: z.string(),
+  JWT_SECRET: z.string(),
+  SENDER_HOST: z.string(),
+  SENDER_PORT: z.coerce.number(),
+  SENDER_USER: z.string(),
+  SENDER_PASS: z.string(),
+  SENDER_FROM: z.string(),
+});
 
 const _env = envSchema.safeParse(process.env);
 
